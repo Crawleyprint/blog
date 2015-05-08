1cf080aa86a8d8500847f2a46d6e5f0fbd0cb53a
Mladen Stepanic <crawleyprint@gmail.com>
2015-05-08 14:45:26 +0200

    Adds inuit starter kit

--- build.js
+++ build.js
@@ -3,13 +3,17 @@
 var metalsmith = require('metalsmith');
 var markdown = require('metalsmith-markdown');
 var templates = require('metalsmith-templates');
+var sass = require('metalsmith-sass');
 
 metalsmith(__dirname)
   .use(markdown())
+  .use(sass({
+    outputDir: 'css'
+  }))
   .use(templates('handlebars'))
   .destination('./build')
   .build(function(err) {
     if (err) {
-      throw err;
+      throw err.message;
     }
   });
