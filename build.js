0b1035dc7dd7ee44d564eac9476aa44225540366
Mladen Stepanic <crawleyprint@gmail.com>
2015-05-09 08:59:15 +0200

    Adds live reload and proper watchers

--- build.js
+++ build.js
@@ -4,8 +4,20 @@ var metalsmith = require('metalsmith');
 var markdown = require('metalsmith-markdown');
 var templates = require('metalsmith-templates');
 var sass = require('metalsmith-sass');
+var watch = require('metalsmith-watch');
+var livereload = require('livereload');
+var connect = require('connect');
+var http = require('http');
+var serveStatic = require('serve-static');
+
+var port = 35729;
+
+
 
 metalsmith(__dirname)
+  .use(watch({
+    livereload: true
+  }))
   .use(markdown())
   .use(sass({
     outputDir: 'css'
@@ -17,3 +29,17 @@ metalsmith(__dirname)
       throw err.message;
     }
   });
+
+/**
+ * create connect app
+ */
+var app = connect();
+
+app
+  .use(require('connect-livereload')({
+    port: port
+  }))
+  .use(serveStatic(__dirname + '/build'));
+
+http.createServer(app).listen(9001);
+
